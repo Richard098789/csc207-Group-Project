42479768c3b58fb960069dbd8d868694b4cf0536
Richard098789 <delfen.xu@mail.utoronto.ca>
2024-11-25 18:08:34 -0500

    Update UserManager to store static information including current login user.

--- src/main/java/Use_case/UserManager.java
+++ src/main/java/Use_case/UserManager.java
@@ -1,27 +1,53 @@
 package Use_case;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
 import entity.User;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import database.FireStoreInitializer;
+import com.google.cloud.firestore.QuerySnapshot;
+
 public class UserManager {
-    private final Map<String, User> users = new HashMap<>(); // Simulates a database
+    private final Map<String, User> allUsers = new HashMap<>(); // Simulates a database
+    public static User currentUser = null;
+    public static Firestore db = FireStoreInitializer.initializeFirestore();
+
+    public void loadUsersFromDB() {
+        try {
+            ApiFuture<QuerySnapshot> data = db.collection("Users").get();
+            for (QueryDocumentSnapshot document : data.get().getDocuments()) {
+                String password = document.getString("password");
+                String username = document.getString("username");
+                User user = new User(username, password);
+                allUsers.put(username, user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public boolean addUser(String username, String password) {
-        if (users.containsKey(username)) {
+        if (allUsers.containsKey(username)) {
             return false; // Username already exists
         }
-        users.put(username, new User(username, password));
+        allUsers.put(username, new User(username, password));
         return true;
     }
 
+    public void setCurrentUser(String username, String password) {
+        currentUser = new User(username, password);
+    }
+
     public boolean validateLogin(String username, String password) {
-        User user = users.get(username);
+        User user = allUsers.get(username);
         return user != null && user.getPassword().equals(password);
     }
 
     public boolean userExists(String username) {
-        return users.containsKey(username);
+        return allUsers.containsKey(username);
     }
 }

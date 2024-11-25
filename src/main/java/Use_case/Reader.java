c63cbb9f69db75e20317539b10751280965d8027
lnick1116 <shengbinnick.li@mail.utoronto.ca>
2024-11-25 00:45:54 -0500

    Implemented Reader.java to read from database.

---
+++ src/main/java/Use_case/Reader.java
@@ -0,0 +1,43 @@
+package Use_case;
+
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import database.FireStoreInitializer;
+import entity.Content;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+public class Reader {
+    static final String PUBLIC_COLLECTION_NAME = "Public";
+
+    public static void fromPublic(Firestore db, String collectionName, String documentId) {
+        try {
+            DocumentSnapshot document = db.collection(collectionName).document(documentId).get().get();
+            Content content = new Content(documentId);
+
+            if (document.exists()) {
+                System.out.println("Document data: " + document.getData());
+                Map<String, Map<String, Object>> map = new HashMap<>();
+                for (String key : document.getData().keySet()) {
+                    Map<String, Object> value = (Map<String, Object>) document.getData().get(key);
+                    map.put(key, value);
+                }
+                content.setContent(map);
+            } else{
+                System.out.println("Document does not exist");
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("Error reading document: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        Firestore db = FireStoreInitializer.initializeFirestore();
+        if (db != null) {
+            Reader.fromPublic(db, "Public", "98765432");
+        }
+    }
+}

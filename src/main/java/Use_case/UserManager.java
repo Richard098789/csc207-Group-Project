97d41b4542fed1eab2dbb5034d0cf2535e83693f
Hallucybanana <delfen.alpha@gmail.com>
2024-11-25 23:55:33 -0500

    Signup function completed, able to perform login and sigup, all user information stored in UserManager

--- src/main/java/Use_case/UserManager.java
+++ src/main/java/Use_case/UserManager.java
@@ -1,17 +1,22 @@
 package Use_case;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
-import entity.User;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.SetOptions;
+import com.google.cloud.firestore.WriteResult;
 
 import database.FireStoreInitializer;
-import com.google.cloud.firestore.QuerySnapshot;
+import entity.User;
 
 public class UserManager {
+    private final static String USER_COLLECTION_NAME = "Users";
+
     private final Map<String, User> allUsers = new HashMap<>(); // Simulates a database
     public static User currentUser = null;
     public static Firestore db = FireStoreInitializer.initializeFirestore();
@@ -25,17 +30,27 @@ public class UserManager {
                 User user = new User(username, password);
                 allUsers.put(username, user);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("Error loading userInfo: " + e.getMessage());
         }
     }
 
     public boolean addUser(String username, String password) {
-        if (allUsers.containsKey(username)) {
-            return false; // Username already exists
+        try {
+            if (allUsers.containsKey(username)) {
+                return false; // Username already exists
+            }
+            User newUser = new User(username, password);
+            allUsers.put(username, newUser); //Update to local usermanager
+            
+            Map<String, String> data = newUser.getSignInInfo();
+            WriteResult result = db.collection(USER_COLLECTION_NAME).document(username).set(data, SetOptions.merge()).get(); // SetOption to merge so that it would not override previous data.
+            return true;
+            
+        } catch (InterruptedException | ExecutionException e) {
+            System.err.println("Error writing document: " + e.getMessage());
+            return false;
         }
-        allUsers.put(username, new User(username, password));
-        return true;
     }
 
     public void setCurrentUser(String username, String password) {

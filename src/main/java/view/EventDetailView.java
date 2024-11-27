3410b1f162640d776019f727ab394c8f1205dae2
lnick1116 <shengbinnick.li@mail.utoronto.ca>
2024-11-27 01:16:47 -0500

    Made Sample display of EventDetailView.java a separate file.

--- src/main/java/view/EventDetailView.java
+++ src/main/java/view/EventDetailView.java
@@ -62,23 +62,4 @@ public class EventDetailView {
 
     }
 
-    // Sample Display
-    public static void main(String[] args) {
-        // Example usage
-        Event event = new Event(
-                "001",
-                "abc",
-                "Concert",
-                "2024-01-01",
-                "2024-01-05",
-                "20:30:00",
-                "Toronto",
-                "001",
-                "Nick",
-                "001",
-                100
-        );
-
-        new EventDetailView(event);
-    }
 }
